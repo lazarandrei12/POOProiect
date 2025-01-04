16b3a0c32b751118d05d400cc2884652f74b8880
Andrei Lazar <lazarandrei639@gmail.com>
2025-01-04 15:37:19 +0200

    rece

--- Program/clase/companie_inchirieri.cs
+++ Program/clase/companie_inchirieri.cs
@@ -4,5 +4,23 @@ public class companie_inchirieri
 {
     public string nume_companie;
     public string adresa;
+    public int cod_unic;
+    List<car> cars = new List<car>();
+    List<Inchirieri> inchiriaza = new List<Inchirieri>();
     
+
+    public companie_inchirieri(string nume_companie, string adresa, int cod_unic)
+    {
+        this.nume_companie = nume_companie;
+        this.adresa = adresa;
+        this.cod_unic = cod_unic;
+        List<car> cars = new List<car>();
+        List<Inchirieri> inchiriaza = new List<Inchirieri>();
+    }
+
+    public override string ToString()
+    {
+        return $"{nume_companie}, {adresa}, {cod_unic}";
+    }
+   
 }
