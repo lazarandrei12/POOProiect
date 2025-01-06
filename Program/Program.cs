9de9b81d109fdff9b4573fab5b3b8bdf2ae1f70c
Andrei Lazar <lazarandrei639@gmail.com>
2025-01-06 17:33:33 +0200

    mzzzzzzzzziiiiii

--- Program/Program.cs
+++ Program/Program.cs
@@ -8,19 +8,20 @@ class Program
     static void Main(string[] args)
     {
         companie_inchirieri companie1 = new companie_inchirieri("pipepepuRent", "timisoara", 12345);
-        client client1 = new client("Belg David", "1234567891023", "belg", "theking");
-        client client2 = new client("Baloshin Darius", "2256784578961", "linganguli", "pasoptist");
-        client client3 = new client("Laser Andrei", "2954828585000", "firmirelu", "rece12");
-        Car car1 = new masina_electric("Tesla", "Y", 2018, 86000, "CJ-15-MIL", true, 130);
-        Car car4 = new masina_standard("BMW", "M3 Competition", 2020, 16000, "TM-21-BLG", true, 230);
-        Car car2 = new masina_electric("Dacia", "Solenza", 2003, 343000, "CJ-99-MUE", true, 70);
-        Car car3 = new masina_standard("Audi", "A5 ", 2015, 50000, "AG-33-SUJ", true, 100);
+        client client1 = new client("viorel", "1234567891023", "sefulBanilor", "rece12");
+        client client2 = new client("mircea", "225678457896", "cing", "431fedf");
+        Car car1 = new masina_electric("tesla ", "y ", 2019, 15, "CJ-15-MUI", true, 40);
+        Car car2 = new masina_electric("dacia ", "bb ", 2021, 2414, "CJ-15-MUI", false, 70);
+        Car car3 = new masina_standard("audee ", "a5 ", 2000, 1500, "AG-23-MEC", true, 150);
         companie1.Adaugamasina(car1);
         companie1.Adaugamasina(car2);
         companie1.Adaugamasina(car3);
-        companie1.Adaugamasina(car4);
-        Inchirieri inchirirere1 = new Inchirieri(client1, car2, DateOnly.Parse("2024-10-5"), DateOnly.Parse("2024-10-15"), true);
-        Inchirieri inchirirere2 = new Inchirieri(client2, car4, DateOnly.Parse("2024-10-18"), DateOnly.Parse("2025-1-25"), false);
+        DateOnly datainceput1 = DateOnly.Parse("2024-10-5");
+        DateOnly datafinal1 = DateOnly.Parse("2024-10-15");
+        DateOnly datainceput2 = DateOnly.Parse("2024-10-18");
+        DateOnly datafinal2 = DateOnly.Parse("2025-1-25");
+        Inchirieri inchirirere1 = new Inchirieri(client1, car2, datainceput1, datafinal1, true);
+        Inchirieri inchirirere2 = new Inchirieri(client2, car3, datainceput2, datafinal2, false);
         companie1.Adaugainchiriere(inchirirere1);
         companie1.Adaugainchiriere(inchirirere2);
         inchirirere1.afiseazadetalii();
@@ -52,6 +53,24 @@ class Program
                     }
 
                     break;
+                
+                case "2":
+                    Console.WriteLine("1.Log in");
+                    Console.WriteLine("2.Sign up");
+                    Console.WriteLine("3.Log in administrator");
+                    Console.WriteLine("Alegeti o optiune: ");
+                    string suboptiune = Console.ReadLine();
+                    break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        break;
                 default:
                     Console.WriteLine("optiune incorecta");
                     break;
